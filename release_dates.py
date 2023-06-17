21add6f690addaa63a78df1e5ac4eb64ad5408e4
Mihai <mcosma@gmail.com>
2023-06-17 17:48:34 -0400

    async

--- release_dates.py
+++ release_dates.py
@@ -1,7 +1,9 @@
 # %%
 import subprocess
-import json
-import requests
+import aiohttp
+from rich.progress import Progress
+import asyncio
+
 import pandas as pd
 import __main__ as main_to_check_for_interactive_mode_only
 
@@ -17,40 +19,56 @@ else:
     display = print  # If not in interactive mode, display is equivalent to print
     INTERACTIVE = False
 
-# Use pip to get list of installed packages
-installed_packages = subprocess.check_output(['pip', 'list']).decode().split('\n')[2:-1]
+async def async_get_release_date(semaphore, session, progress, task_id, package):
+    async with semaphore:
+        # Make a request to PyPi's JSON API for the package
+        async with session.get(f'https://pypi.org/pypi/{package}/json') as response:
+            # If the response status is not 200, the package was not found on PyPi
+            if response.status != 200:
+                print(f'Package {package} not found on PyPi.')
+                return {'Package': package, 'Latest Version': 'not found', 'Release Date': 'not found'}
 
-# Parse the output to get package names
-package_names = [pkg.split()[0] for pkg in installed_packages]
+            # Parse the JSON response
+            data = await response.json()
 
-# Initialize an empty DataFrame
-df = pd.DataFrame(columns=['Package', 'Release Date'])
+            # Get the release date of the latest version
+            latest_version = data['info']['version']
+            release_date = data['releases'][data['info']['version']][0]['upload_time']
 
-records = []
-# Loop through all packages
-for package in package_names:
-    # Make a request to PyPi's JSON API for the package
-    response = requests.get(f'https://pypi.org/pypi/{package}/json')
+            # Update the progress bar
+            progress.update(task_id, advance=1)
 
-    # If the response status is not 200, the package was not found on PyPi
-    if response.status_code != 200:
-        print(f'Package {package} not found on PyPi.')
-        continue
+            return {'Package': package, 'Latest Version': latest_version, 'Release Date': release_date}
 
-    # Parse the JSON response
-    data = json.loads(response.text)
+async def async_get_release_dates(package_names, concurrency=5):
+    # Create a Semaphore for the specified concurrency
+    semaphore = asyncio.Semaphore(concurrency)
+    progress = Progress()
+    progress.start()
 
-    # Get the release date of the latest version
-    latest_version = data['info']['version']
-    release_date = data['releases'][data['info']['version']][0]['upload_time']
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for package in package_names:
+            task_id = progress.add_task(f"[cyan]{package}...", total=1)
+            tasks.append(async_get_release_date(semaphore, session, progress, task_id, package))
 
-    # Add the package and release date to the DataFrame
-    # df = df.append({'Package': package, 'Latest Version': latest_version, 'Release Date': release_date}, ignore_index=True)
-    records.append({'Package': package, 'Latest Version': latest_version, 'Release Date': release_date})
+        results = await asyncio.gather(*tasks)
 
-# Print the DataFrame
-df = pd.DataFrame(records).sort_values(by='Release Date', ascending=False)
+    progress.stop()
 
-# %%
-df_str = df.head(10).to_string(index=False)
-display(df_str)
+    return pd.DataFrame(results).sort_values('Release Date', ascending=False)
+
+
+async def main():
+    # Use pip to get list of installed packages
+    installed_packages = subprocess.check_output(['pip', 'list']).decode().split('\n')[2:-1]
+
+    # Parse the output to get package names
+    package_names = [pkg.split()[0] for pkg in installed_packages]
+
+    # get the release dates
+    df = await async_get_release_dates(package_names)
+    print(f"{df=}")
+
+if __name__ == '__main__':
+    asyncio.run(main())
